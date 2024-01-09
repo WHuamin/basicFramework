27289e277c89257464cb4b2534a52bb2439fccea
WangHuamin <987599815@qq.com>
2024-01-09 14:32:29 +0800

    feat: 项目基础搭建，包含配置ts，代码风格优化统一，提交代码检测规范等

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from "path"; //这个path用到了依赖@types/node 【pnpm i @types/node -D】
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  //这里进行配置别名
+  resolve: {
+    alias: {
+      "@": path.resolve("./src"), // @代替src
+      "#": path.resolve("./types"), // #代替types
+    },
+  },
+})
