ccd851c07837c6a88ce40a629bab84b553855605
WangHuamin <987599815@qq.com>
2024-01-11 10:11:13 +0800

    feat: 优化路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import routes from './routes';
+import type { App } from 'vue';
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+// 引入 common.ts
+import CommonRouter from './modules/common';
+import ManageRouter from './modules/manage';
 import { useUserStore } from '@/store/user';
+
+export const publicRoutes: Array<RouteRecordRaw> = [...CommonRouter, ...ManageRouter];
 
 const router = createRouter({
   history: createWebHistory(), //可传参数，配置base路径，例如'/app'
-  routes,
+  routes: publicRoutes,
 });
 
 router.beforeEach((to, from, next) => {
@@ -25,3 +30,18 @@ router.beforeEach((to, from, next) => {
 });
 
 export default router;
+
+/* 初始化路由表 */
+export function resetRouter() {
+  router.getRoutes().forEach((route) => {
+    const { name } = route;
+    if (name) {
+      router.hasRoute(name) && router.removeRoute(name);
+    }
+  });
+}
+
+/* 导出 setupRouter */
+export const setupRouter = (app: App<Element>) => {
+  app.use(router);
+};
